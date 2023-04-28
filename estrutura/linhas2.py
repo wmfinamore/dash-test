e87859907238458be3f0c6c7728fbd272d84335d
Willian Moreira Finamore <wmfinamore@hotmail.com>
2023-04-28 16:55:54 -0300

    Outro exemplo de implementação usando Row do bootstrap no Dash

---
+++ estrutura/linhas2.py
@@ -0,0 +1,43 @@
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP])
+
+app.layout = html.Div([
+            dbc.Row(html.Div([
+                html.H1('Meu Programa'),
+                html.Label('Cadastro de Alunos')],
+                            style = {'background-color': '#FCD6A8',
+                                    'width':'500px',
+                                    'height':'90px',})),
+            dbc.Row(html.Div(['E-mail: ',
+                            dcc.Input(id = 'entrada_01',
+                                        value = '',
+                                        type = 'text'),
+                            html.Label('@escola.com')],
+                            style = {'background-color':'#FCF4A9',
+                                    'width':'500px',
+                                    'height':'30px',})),
+            html.P(),
+            html.Div(id = 'saida_01',
+                    style = {
+                        'background-color':'lightblue',
+                        'font-family':'verdana',
+                        'font-size':'16px',
+                        'padding':'10px',
+                        'width':'300px',
+                        'height':'100px',
+                        'border':'20px solid powderblue',
+                        'text-align':'center',
+                        'margin':'10px'
+            }),
+])
+
+@app.callback(Output(component_id = 'saida_01', component_property = 'children'),
+              [Input(component_id = 'entrada_01', component_property = 'value')])
+def mostra_nome(e):
+    return f'Bem vindo(a) {e}!!!'
+
+app.run(debug=True)
